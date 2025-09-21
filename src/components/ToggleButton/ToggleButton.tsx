4d88d4d844963c5050cd4757ff0c64f24b9cc628
hadodev <hado.dev.app@gmail.com>
2025-09-21 19:11:56 +0200

    accessibility html

--- src/components/ToggleButton/ToggleButton.tsx
+++ src/components/ToggleButton/ToggleButton.tsx
@@ -3,7 +3,7 @@ import styles from '@components/ToggleButton/ToggleButton.module.css'
 const ToggleButton = () => {
     return (
         <div className={styles.tglBtn}>
-            <input name="color-scheme" type="checkbox" defaultChecked id="color-scheme-toggle" />
+            <input name="color-scheme" type="checkbox" defaultChecked id="color-scheme-toggle" aria-label="toggle color scheme" />
             <label htmlFor="color-scheme-toggle"></label>
         </div>
     )
