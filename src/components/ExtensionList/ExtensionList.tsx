bfa6f19ef8f77409f55c3d53ea148c713ceb13f0
hadodev <hado.dev.app@gmail.com>
2025-09-21 18:25:48 +0200

    Fixed bugs

--- src/components/ExtensionList/ExtensionList.tsx
+++ src/components/ExtensionList/ExtensionList.tsx
@@ -1,6 +1,6 @@
 import type { Extension } from '../../types'
 import Card from '@components/Card/Card'
-import styles from ' @components/ExtensionList/ExtensionList.module.css'
+import styles from '@components/ExtensionList/ExtensionList.module.css'
 
 interface ExtensionListProps {
   extensions: Extension[]
